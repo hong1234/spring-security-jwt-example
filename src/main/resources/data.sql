1d948ab199818136d46fc84f99a810ddb89a3abd
hong <hong@hong-510-p100ng.net>
2020-07-11 14:52:22 +0200

    dockerized Spring boot application

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
-INSERT INTO `user_tbl` (`id`, `email`, `password`, `role`, `username`) VALUES
+delete from user_tbl;
+
+INSERT INTO user_tbl (id, email, password, role, username) VALUES
 (1, 'abc@yahoo.de', '$2a$10$gZ0vDZzmj8GmQQVclvs/nuzK0.Ct605hoEayew/IBsRbjrFLEmkxa', 'ROLE_USER', 'hong'),
 (2, 'abc2@yahoo.de', '$2a$10$zE3m3UTp5koU4KAaminN2eAGS9hfJpPIfcrJ5/iwXcYbp6lmzybS6', 'ROLE_USER, ROLE_ADMIN', 'hong2');
